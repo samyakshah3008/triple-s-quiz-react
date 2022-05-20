48a1d6c47cc7aea70ad54b74f03285eb38eddb88
Samyak Shah <samyakshah3008@gmail.com>
2022-05-20 21:00:08 +0530

    FEAT: Navbar component add

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <h1 className="nav-header center margin-top">TripleS Quiz</h1>
+      </nav>
+    </div>
+  );
+}
