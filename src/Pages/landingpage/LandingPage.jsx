00e6e5698d6fff16cf081a1886e96c1c10c80ff1
Samyak Shah <samyakshah3008@gmail.com>
2022-05-24 01:46:30 +0530

    minor update

--- src/Pages/landingpage/LandingPage.jsx
+++ src/Pages/landingpage/LandingPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Card from "../../Components/Card";
-import Header from "../../Components/Header";
 import NavBar from "../../Components/NavBar";
 import "./landingpage.css";
 export default function LandingPage() {
   return (
     <div>
       <NavBar />
-      <Header />
       <Card />
     </div>
   );
