c8522e627e3e650b9e6caaf1aa74af07502b3200
Samyak Shah <samyakshah3008@gmail.com>
2022-05-23 18:05:19 +0530

    Question page component integrated

---
+++ src/Pages/questionpage/QuestionPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Questions from "../../Components/Questions";
+
+export default function QuestionPage() {
+  return (
+    <div>
+      <Questions></Questions>
+    </div>
+  );
+}
