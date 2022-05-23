d30926db0898717a81245a47b5acec64d4fa6206
Samyak Shah <samyakshah3008@gmail.com>
2022-05-24 01:47:02 +0530

    resultpage component integrated

--- src/Pages/resultpage/ResultPage.jsx
+++ src/Pages/resultpage/ResultPage.jsx
@@ -1,46 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useQuiz } from "../../contexts/quizContext";
+import "./resultpage.css";
 
 export default function ResultPage() {
   const { quizState, quizDispatch } = useQuiz();
   const [currScore, setCurrScore] = useState(0);
   const { selectedAnswers, activeQuiz, activeQuizAnswers } = quizState;
-  const getClassName = (currOption, index) => {
+  const getclassNameName = (currOption, index) => {
     if (activeQuizAnswers[index] === currOption) return "correct";
     if (selectedAnswers[index] === currOption) return "wrong";
     return "";
   };
 
+  useEffect(() => {
+    let innerCurrScore = 0;
+    for (let i = 0; i < activeQuizAnswers.length; i++)
+      if (activeQuizAnswers[i] === selectedAnswers[i]) innerCurrScore += 20;
+    setCurrScore(innerCurrScore);
+  }, []);
+
   return (
     <div>
       <nav>
-        <h1 class="nav-header center margin-top">Results</h1>
-        <div class="nav-header center margin-top-xl">Final Score: 20/50</div>
+        <h1 className="nav-header center margin-top">Results</h1>
+        <div className="nav-header center margin-top-xl">
+          Final Score: {currScore} /100
+        </div>
+        <hr style={{ marginTop: "30px", width: "600px", margin: "auto" }} />
       </nav>
 
       <main>
-        <div class="question-container margin-top-xl">
-          <div class="question text-white  margin-top">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-            aspernatur laudantium excepturi velit maiores quia non rem placeat
-            ad esse?
-          </div>
-
-          {quiz.options.map((option) => (
-            <button
-              type="radio"
-              className={`option ${getClassName(option, index)}`}
-              name="option"
-            >
-              {option}
-            </button>
+        <div className="question-container margin-top-xl">
+          {activeQuiz.map((quiz, index) => (
+            <>
+              <div className="question text-white margin-top">
+                {quiz.Statement}
+              </div>
+              <div className="question-options-container margin-top">
+                {quiz.options.map((option) => (
+                  <button
+                    type="radio"
+                    classNameName={`options ${getclassNameName(option, index)}`}
+                    name="option"
+                    key={option}
+                  >
+                    {option}
+                  </button>
+                ))}
+              </div>
+              <hr style={{ marginTop: "30px" }} />
+            </>
           ))}
 
-          <div class="footer margin-top-xl margin-bottom-xl">
-            <button class="home-btn bold">Play more</button>
-            <button class="twitter-btn bold">Twitter</button>
-            <button class="github-btn bold">Github</button>
-            <button class="linkedin-btn bold">Linkedin</button>
+          <div className="footer margin-top-xl margin-bottom-xl">
+            <Link to="/">
+              <button className="home-btn bold">Play more</button>
+            </Link>
+            <button className="twitter-btn bold">Twitter</button>
+            <button className="github-btn bold">Github</button>
+            <button className="linkedin-btn bold">Linkedin</button>
           </div>
         </div>
       </main>
