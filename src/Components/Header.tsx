91931a73df2d4743f6630cd2a1ad4ded4806c8bd
Samyak Shah <samyakshah3008@gmail.com>
2022-06-21 16:18:27 +0530

    DOCS: tsx header file add

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="level-container margin-top">
+        <button className="btn easy-level easy-level-active">Easy</button>
+        <button className="btn medium-level">Medium</button>
+        <button className="btn hard-level">Hard</button>
+      </div>
+    </div>
+  );
+}
